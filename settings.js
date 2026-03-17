7fb01b51423ae299a41cb01fd2743ac290fbfd1c
maabsirat-jpg <maabsirat@gmail.com>
2026-03-17 06:27:39 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "MS bor",
+  botOwner: '+1736377885', // Your name
+  ownerNumber: '+93771540141', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
